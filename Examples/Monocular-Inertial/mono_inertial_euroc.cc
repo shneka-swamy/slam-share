d42d76a1ccb0954e0015af0704c7851f0aad64e3
shneka-swamy <shneka.swamy@gmail.com>
2025-04-01 18:00:15 -0400

    Checking using monocular inertial

--- Examples/Monocular-Inertial/mono_inertial_euroc.cc
+++ Examples/Monocular-Inertial/mono_inertial_euroc.cc
@@ -40,6 +40,7 @@ double ttrack_tot = 0;
 int main(int argc, char *argv[])
 {
 
+    cout <<"Called int main"<<endl;
     if(argc < 5)
     {
         cerr << endl << "Usage: ./mono_inertial_euroc path_to_vocabulary path_to_settings path_to_sequence_folder_1 path_to_times_file_1 (path_to_image_folder_2 path_to_times_file_2 ... path_to_image_folder_N path_to_times_file_N) " << endl;
