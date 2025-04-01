ca5b1d736fb038f0546941465e2d48a930698092
shneka_swamy <shneka_swamy@gmail.com>
2025-04-01 05:35:58 +0000

    dunno how to use overrides in vcpkg.json

---
+++ vcpkg_installed/x64-linux/share/pangolin/PangolinTargets-debug.cmake
@@ -0,0 +1,159 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "pango_core" for configuration "Debug"
+set_property(TARGET pango_core APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(pango_core PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libpango_core.a"
+  )
+
+list(APPEND _cmake_import_check_targets pango_core )
+list(APPEND _cmake_import_check_files_for_pango_core "${_IMPORT_PREFIX}/debug/lib/libpango_core.a" )
+
+# Import target "pango_display" for configuration "Debug"
+set_property(TARGET pango_display APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(pango_display PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libpango_display.a"
+  )
+
+list(APPEND _cmake_import_check_targets pango_display )
+list(APPEND _cmake_import_check_files_for_pango_display "${_IMPORT_PREFIX}/debug/lib/libpango_display.a" )
+
+# Import target "pango_geometry" for configuration "Debug"
+set_property(TARGET pango_geometry APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(pango_geometry PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libpango_geometry.a"
+  )
+
+list(APPEND _cmake_import_check_targets pango_geometry )
+list(APPEND _cmake_import_check_files_for_pango_geometry "${_IMPORT_PREFIX}/debug/lib/libpango_geometry.a" )
+
+# Import target "pango_glgeometry" for configuration "Debug"
+set_property(TARGET pango_glgeometry APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(pango_glgeometry PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libpango_glgeometry.a"
+  )
+
+list(APPEND _cmake_import_check_targets pango_glgeometry )
+list(APPEND _cmake_import_check_files_for_pango_glgeometry "${_IMPORT_PREFIX}/debug/lib/libpango_glgeometry.a" )
+
+# Import target "pango_image" for configuration "Debug"
+set_property(TARGET pango_image APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(pango_image PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libpango_image.a"
+  )
+
+list(APPEND _cmake_import_check_targets pango_image )
+list(APPEND _cmake_import_check_files_for_pango_image "${_IMPORT_PREFIX}/debug/lib/libpango_image.a" )
+
+# Import target "pango_opengl" for configuration "Debug"
+set_property(TARGET pango_opengl APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(pango_opengl PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libpango_opengl.a"
+  )
+
+list(APPEND _cmake_import_check_targets pango_opengl )
+list(APPEND _cmake_import_check_files_for_pango_opengl "${_IMPORT_PREFIX}/debug/lib/libpango_opengl.a" )
+
+# Import target "pango_packetstream" for configuration "Debug"
+set_property(TARGET pango_packetstream APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(pango_packetstream PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libpango_packetstream.a"
+  )
+
+list(APPEND _cmake_import_check_targets pango_packetstream )
+list(APPEND _cmake_import_check_files_for_pango_packetstream "${_IMPORT_PREFIX}/debug/lib/libpango_packetstream.a" )
+
+# Import target "pango_plot" for configuration "Debug"
+set_property(TARGET pango_plot APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(pango_plot PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libpango_plot.a"
+  )
+
+list(APPEND _cmake_import_check_targets pango_plot )
+list(APPEND _cmake_import_check_files_for_pango_plot "${_IMPORT_PREFIX}/debug/lib/libpango_plot.a" )
+
+# Import target "pango_python" for configuration "Debug"
+set_property(TARGET pango_python APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(pango_python PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libpango_python.a"
+  )
+
+list(APPEND _cmake_import_check_targets pango_python )
+list(APPEND _cmake_import_check_files_for_pango_python "${_IMPORT_PREFIX}/debug/lib/libpango_python.a" )
+
+# Import target "pango_scene" for configuration "Debug"
+set_property(TARGET pango_scene APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(pango_scene PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libpango_scene.a"
+  )
+
+list(APPEND _cmake_import_check_targets pango_scene )
+list(APPEND _cmake_import_check_files_for_pango_scene "${_IMPORT_PREFIX}/debug/lib/libpango_scene.a" )
+
+# Import target "pango_tools" for configuration "Debug"
+set_property(TARGET pango_tools APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(pango_tools PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libpango_tools.a"
+  )
+
+list(APPEND _cmake_import_check_targets pango_tools )
+list(APPEND _cmake_import_check_files_for_pango_tools "${_IMPORT_PREFIX}/debug/lib/libpango_tools.a" )
+
+# Import target "pango_vars" for configuration "Debug"
+set_property(TARGET pango_vars APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(pango_vars PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libpango_vars.a"
+  )
+
+list(APPEND _cmake_import_check_targets pango_vars )
+list(APPEND _cmake_import_check_files_for_pango_vars "${_IMPORT_PREFIX}/debug/lib/libpango_vars.a" )
+
+# Import target "pango_video" for configuration "Debug"
+set_property(TARGET pango_video APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(pango_video PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libpango_video.a"
+  )
+
+list(APPEND _cmake_import_check_targets pango_video )
+list(APPEND _cmake_import_check_files_for_pango_video "${_IMPORT_PREFIX}/debug/lib/libpango_video.a" )
+
+# Import target "pango_windowing" for configuration "Debug"
+set_property(TARGET pango_windowing APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(pango_windowing PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C;CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libpango_windowing.a"
+  )
+
+list(APPEND _cmake_import_check_targets pango_windowing )
+list(APPEND _cmake_import_check_files_for_pango_windowing "${_IMPORT_PREFIX}/debug/lib/libpango_windowing.a" )
+
+# Import target "tinyobj" for configuration "Debug"
+set_property(TARGET tinyobj APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(tinyobj PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libtinyobj.a"
+  )
+
+list(APPEND _cmake_import_check_targets tinyobj )
+list(APPEND _cmake_import_check_files_for_tinyobj "${_IMPORT_PREFIX}/debug/lib/libtinyobj.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
