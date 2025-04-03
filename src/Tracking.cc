3fee75b502e9c7dc3788863fe4ce39736587aec7
shneka-swamy <shneka.swamy@gmail.com>
2025-04-03 17:03:44 -0400

    Adding cout to check why the loop is not released

--- src/Tracking.cc
+++ src/Tracking.cc
@@ -3801,6 +3801,7 @@ void Tracking::ResetActiveMap(bool bLocMap)
         Verbose::PrintMess("done", Verbose::VERBOSITY_NORMAL);
     }
 
+    std::cout<<"Calling the reset for loop closure -- came out of the mapper loop" << std::endl;
     // Reset Loop Closing
     Verbose::PrintMess("Reseting Loop Closing...", Verbose::VERBOSITY_NORMAL);
     mpLoopClosing->RequestResetActiveMap(pMap);
